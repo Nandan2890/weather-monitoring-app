0f9ecfdaf315e924ea14cb19f9ec92cce4230c35
Nandan Kumar Bharti <76986715+Nandan2890@users.noreply.github.com>
2024-10-25 19:58:32 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,11 +2,11 @@ const url = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const geoApiOptions = {
   method: "GET",
   headers: {
-    "x-rapidapi-key": "8237c36dcamsh08b83dd57ad4304p1f6784jsn77c1b55b3cea",
+    "x-rapidapi-key": "",
     "x-rapidapi-host": "wft-geo-db.p.rapidapi.com",
   },
 };
 
 export const GEO_API_URL = url;
-export const WEATHER_API_KEY = "b2bf716c9a0fae77eac084cfdf6fd64a";
+export const WEATHER_API_KEY = "";
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
